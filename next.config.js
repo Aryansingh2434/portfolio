bdafd4254d8c847e5f5a8a760afe83168e3ff246
Aryan Singh <chauhanaryan011@gmail.com>
2025-04-12 17:38:18 +0530

    Init

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['raw.githubusercontent.com'], // Add any other domains you're loading images from
+  },
+  typescript: {
+    ignoreBuildErrors: true, // Only use this if you want to ignore TypeScript errors during build
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // Only use this if you want to ignore ESLint errors during build
+  }
+}
 
 module.exports = nextConfig
